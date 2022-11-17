0e4dff8bec33dbe0c57ffb9bb3a46a0351b5b009
Gutinnho <glunardi99@gmail.com>
2022-11-17 15:14:26 -0300

    Arrumando o bug de scroll lateral

--- src/components/Patas/index.tsx
+++ src/components/Patas/index.tsx
@@ -1,12 +1,26 @@
 import styles from './Patas.module.scss';
 
-import {ReactComponent as PatasSvg} from 'assets/Patas.svg';
+import PatasP from 'assets/Patas-P.png';
+import PatasM from 'assets/Patas-M.png';
+import PatasG from 'assets/Patas-G.png';
 
 function Patas(){
 	return(
 		<div className={styles.patas}>
-			<PatasSvg 
-				className={styles.patas__svg}
+			<img 
+				src={PatasP} 
+				alt="" 
+				className={styles.patas__P} 
+			/>
+			<img
+				src={PatasM}
+				alt=""
+				className={styles.patas__M}
+			/>
+			<img
+				src={PatasG}
+				alt=""
+				className={styles.patas__G}
 			/>
 		</div>
 	);
